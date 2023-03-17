84f07b90d5e3364c9b92baafbf813a41247439b5
Lyric <5h3ll3x@gmail.com>
2023-03-17 15:43:07 +0900

    add missing file

---
+++ console/src/components/botastic/BotasticCreditPanel.vue
@@ -0,0 +1,35 @@
+<template>
+  <VSheet class="mb-4 rounded-sm" color="greyscale_7" elevation="1">
+    <div class="d-flex align-end px-4 pt-1 pb-2">
+      <div>
+        <div class="text-overline">{{ t("credits") }}</div>
+        <div class="text-h5">{{ credits }}</div>
+      </div>
+      <VSpacer />
+      <FButton color="greyscale_2" class="rounded-sm" disabled>{{ t("topup") }}</FButton>
+    </div>
+    <div class="text-right text-caption text-greyscale_3 px-4 pb-2">
+      {{ t("botastic.credits.hint_disabled") }}
+    </div>
+  </VSheet>
+</template>
+
+<script lang="ts">
+export default {
+  name: "BotasticCreditPanel",
+};
+</script>
+
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+const { t } = useI18n({ useScope: "local" });
+
+const botasticDataStore = useBotasticDataStore();
+
+const  { credits } = storeToRefs(botasticDataStore);
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
