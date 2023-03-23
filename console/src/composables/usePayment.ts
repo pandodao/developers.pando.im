1a68b4b20995a37b7721b9d4c4463cb5c75c451a
Lyric <5h3ll3x@gmail.com>
2023-03-23 16:17:47 +0900

    fix import error

--- console/src/composables/usePayment.ts
+++ console/src/composables/usePayment.ts
@@ -1,9 +1,6 @@
-import Mixpay from "mixpayjs";
 import { placeOrderMixpay } from "../services/botastic";
 
 export function usePayment() {
-  const mixpayClient = Mixpay();
-
   const payWithMixpay = async (amount: string) => {
     const resp = await placeOrderMixpay(amount)
     const paymentLink = 'https://mixpay.me/code/' + resp.code;
