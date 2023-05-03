9c7ca857b3772cf3b114131b6201044f714e758b
Lyric <5h3ll3x@gmail.com>
2023-05-03 18:03:00 +0900

    minor fix

--- console/src/composables/usePayment.ts
+++ console/src/composables/usePayment.ts
@@ -3,8 +3,7 @@ import { placeOrderLemon, placeOrderMixpay } from "../services/botastic";
 export function usePayment() {
   const payWithMixpay = async (amount: string) => {
     const resp = await placeOrderMixpay(amount, window.location.href)
-    const paymentLink = 'https://mixpay.me/code/' + resp.code;
-    window.location.href = paymentLink;
+    window.location.href = resp.payment_url;
   };
   const payWithLemon = async (varianId: number) => {
     const resp = await placeOrderLemon(varianId, window.location.href)
